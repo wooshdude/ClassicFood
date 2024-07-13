5709cf03b689e92e48f590bd817c391cbef4c091
wooshdude <parker@cline.cc>
2024-07-13 06:02:37 -0700

    Offset armor level GUI
    Offset the armor level GUI element to be more in-line with classic Minecraft's interface.

--- src/main/java/com/mrbysco/classicfood/ClassicFood.java
+++ src/main/java/com/mrbysco/classicfood/ClassicFood.java
@@ -4,8 +4,10 @@ import com.mrbysco.classicfood.client.ClientHandler;
 import com.mrbysco.classicfood.config.ClassicFoodConfig;
 import com.mrbysco.classicfood.handler.PoisonHandler;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -16,18 +18,42 @@ import org.apache.logging.log4j.Logger;
 
 @Mod(ClassicFood.MOD_ID)
 public class ClassicFood {
-	public static final Logger LOGGER = LogManager.getLogger();
-	public static final String MOD_ID = "classicfood";
 
-	public ClassicFood() {
-		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ClassicFoodConfig.commonSpec);
-		eventBus.register(ClassicFoodConfig.class);
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "classicfood";
 
-		MinecraftForge.EVENT_BUS.addListener(PoisonHandler::onPlayerTick);
+    public ClassicFood() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModLoadingContext.get()
+            .registerConfig(
+                ModConfig.Type.COMMON,
+                ClassicFoodConfig.commonSpec
+            );
+        eventBus.register(ClassicFoodConfig.class);
 
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
-			MinecraftForge.EVENT_BUS.addListener(ClientHandler::onGameOverlayRender);
-		});
-	}
+        MinecraftForge.EVENT_BUS.addListener(PoisonHandler::onPlayerTick);
+
+        DistExecutor.unsafeRunWhenOn(
+            Dist.CLIENT,
+            () ->
+                () -> {
+                    MinecraftForge.EVENT_BUS.addListener(
+                        ClientHandler::onGameOverlayRender
+                    );
+                }
+        );
+    }
+
+    @Mod.EventBusSubscriber(
+        modid = MOD_ID,
+        bus = Mod.EventBusSubscriber.Bus.MOD,
+        value = Dist.CLIENT
+    )
+    public static class ClientModEvents {
+
+        @SubscribeEvent
+        public static void registerGuiOverlays(RegisterGuiOverlaysEvent event) {
+            event.registerAboveAll("armor_level", ClientHandler.ARMOR_LEVEL);
+        }
+    }
 }
--- src/main/java/com/mrbysco/classicfood/client/ClientHandler.java
+++ src/main/java/com/mrbysco/classicfood/client/ClientHandler.java
@@ -1,13 +1,69 @@
 package com.mrbysco.classicfood.client;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.gui.overlay.GuiOverlayManager;
+import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 
 public class ClientHandler {
-	public static void onGameOverlayRender(RenderGuiOverlayEvent.Pre event) {
-		if (event.getOverlay() == GuiOverlayManager.findOverlay(new ResourceLocation("food_level"))) {
-			event.setCanceled(true);
-		}
-	}
+
+    protected static final ResourceLocation ARMOR_FULL = new ResourceLocation(
+        "minecraft",
+        "textures/gui/sprites/hud/armor_full.png"
+    );
+    protected static final ResourceLocation ARMOR_HALF = new ResourceLocation(
+        "minecraft",
+        "textures/gui/sprites/hud/armor_half.png"
+    );
+    protected static final ResourceLocation ARMOR_EMPTY = new ResourceLocation(
+        "minecraft",
+        "textures/gui/sprites/hud/armor_empty.png"
+    );
+
+    public static void onGameOverlayRender(RenderGuiOverlayEvent.Pre event) {
+        if (
+            event.getOverlay() ==
+            GuiOverlayManager.findOverlay(new ResourceLocation("food_level"))
+        ) {
+            event.setCanceled(true);
+        }
+        if (
+            event.getOverlay() ==
+            GuiOverlayManager.findOverlay(new ResourceLocation("armor_level"))
+        ) {
+            event.setCanceled(true);
+        }
+    }
+
+    public static final IGuiOverlay ARMOR_LEVEL = (
+        gui,
+        guiGraphics,
+        partialTick,
+        screenWidth,
+        screenHeight
+    ) -> {
+        Minecraft minecraft = Minecraft.getInstance();
+        minecraft.getProfiler().push("armor");
+        RenderSystem.enableBlend();
+        int left = screenWidth / 2 + 10;
+        int top = screenHeight - gui.rightHeight;
+        int level = minecraft.player.getArmorValue();
+
+        for (int i = 1; level > 0 && i < 20; i += 2) {
+            if (i < level) {
+                guiGraphics.blit(ARMOR_FULL, left, top, 0, 0, 9, 9, 9, 9);
+            } else if (i == level) {
+                guiGraphics.blit(ARMOR_HALF, left, top, 0, 0, 9, 9, 9, 9);
+            } else if (i > level) {
+                guiGraphics.blit(ARMOR_EMPTY, left, top, 0, 0, 9, 9, 9, 9);
+            }
+
+            left += 8;
+        }
+
+        RenderSystem.disableBlend();
+        minecraft.getProfiler().pop();
+    };
 }
